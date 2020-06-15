b96f22a8d458c4b29ad404104fb6279a27e80333
coelho-hotmart <coelho@hotmart.com>
2020-06-14 21:57:25 -0300

    remove files

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 
 import { View } from 'react-native';
 import { Container, TypeTitle, TypeDescription, TypeImage, RequestButton, RequestButtonText } from './style';
-import uberx from '../../assets/uberx.png';
 
 export default class Details extends Component {
     render() {
         return (
             <Container>
                 <TypeTitle>O CUSTO DO FRETE SERÁ</TypeTitle>
-                <TypeDescription>Viagens baratas para o dia a dia</TypeDescription>
-                <TypeTitle>Uber X</TypeTitle>
-                <RequestButton onPress={() => { }}>
-                    <RequestButtonText>SOLICITAR UBER X</RequestButtonText>
-                </RequestButton>
             </Container>
         );
     }
